1c683b9d4f1f317d2bdfc387d215054f03b9c8e5
Steven Perfors <stevecanbuild@gmail.com>
2020-01-27 16:41:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if 
+    if age 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
